3373e8aeaae4671e942f3089ac93a4c79b59647e
Rafi <rkhaled@vassar.edu>
2018-03-17 02:10:15 -0400

    working

--- analysis.py
+++ analysis.py
@@ -11,8 +11,8 @@ def main():
     #create_plot_all(savefig = True)
     #create_plot_cdf(showfig = True)
     #create_plot_cdf(showfig = True, day = "2018-02-11 Sunday")
-    #create_plot_cdf(showfig = True, day = "2018-02-12 Monday")
-    #create_plot_cdf(showfig = True, day = "2018-02-13 Tuesday")
+    create_plot_cdf(showfig = True)
+    create_plot_cdf(showfig = True, day = "2018-02-13 Tuesday")
     create_plot_cdf(site = "www.google.com", showfig = True)
     create_plot_cdf(site = "www.google.com", showfig = True)
 
@@ -23,13 +23,13 @@ def get(site):
 #a separate log file for each website in the
 #directory folder logs.
 def makeFiles():
-    f = open('aggregatelog.txt', 'r')
+    f = open('newtestaggregatelog.txt', 'r')
     website_logs = collections.defaultdict(list)
     for line in f.readlines():
         values = line.split(',')
         sitename = values[1].replace("https://", "").replace("/", "-")
         time = values[0]
-        RTTsthree = values[-4].replace['\n','']
+        RTTsthree = values[-4].replace('\n','')
         RTT = values[-3].replace('\n','')
         RTTstwo = values[-2].replace('\n','')
         pRTT = values[-1].replace('\n','')
@@ -69,21 +69,24 @@ def get_values(site = "aggregatelog.txt", day = ""):
     for line in f.readlines():
         values = line.split(',')
         if day == "" :
+            RTTv3s.append(values[-4].replace('\n',''))
             RTTs.append(values[-3].replace('\n',''))
             RTTv2s.append(values[-2].replace('\n',''))
             pR.append(values[-1].replace('\n',''))
         elif day[:10] in values[0]:
+            RTTv3s.append(values[-4].replace('\n',''))
             RTTs.append(values[-3].replace('\n',''))
             RTTv2s.append(values[-2].replace('\n',''))
             pR.append(values[-1].replace('\n',''))
 
 
-    pR = [x for x in pR if x.replace('.','',1).isdigit()]
+    RTTv3s = [x for x in RTTv3s if x.replace('.','',1).isdigit()]
     RTTs = [x for x in RTTs if x.replace('.','',1).isdigit()]
     RTTv2s = [x for x in RTTv2s if x.replace('.','',1).isdigit()]
+    pR = [x for x in pR if x.replace('.','',1).isdigit()]
 
     f.close()
-    return (RTTs, RTTv2s, pR)
+    return (RTTv3s, RTTs, RTTv2s, pR)
 
 def get_location(site):
     cwd = os.getcwd()
@@ -129,24 +132,29 @@ def create_plot_cdf(site = "aggregatelog.txt", savefig = False, showfig = False
 
     vals = get_values(site, day)
 
-    RTT = sort_and_cast(vals[0])
-    RTTv2 = sort_and_cast(vals[1])
-    pR = sort_and_cast(vals[2])
+    RTTv3 = sort_and_cast(vals[0])
+    RTT = sort_and_cast(vals[1])
+    RTTv2 = sort_and_cast(vals[2])
+    pR = sort_and_cast(vals[3])
 
-    plt.plot(np.sort(RTT), np.linspace(0, 1, len(RTT), endpoint=False))
+
+
     plt.plot(np.sort(pR), np.linspace(0, 1, len(pR), endpoint=False))
+    plt.plot(np.sort(RTT), np.linspace(0, 1, len(RTT), endpoint=False))
     plt.plot(np.sort(RTTv2), np.linspace(0, 1, len(RTTv2), endpoint=False))
+    plt.plot(np.sort(RTTv3), np.linspace(0, 1, len(RTTv2), endpoint=False))
     plt.xlabel('RTT (ms)')
     plt.ylabel('CDF')
     if site == 'aggregatelog.txt':
         plt.title('Aggregate CDF for RTTs of the top 100 websites (%i) on %s'
-        % (len(RTT) + len(RTTv2) + len(pR), day))
+        % (len(RTTv3) + len(RTT) + len(RTTv2) + len(pR), day))
     else:
         plt.title('CDF for RTTs of %s (%i) on %s'
-        % (site, len(RTT) + len(RTTv2) + len(pR), day))
-    plt.legend(['TTFB - PRET', 'ping RTT', 'TTFB - PRET 2ND LOAD'])
+        % (site,len(RTTv3) +  len(RTT) + len(RTTv2) + len(pR), day))
+    #plt.legend(['TTFB - PRET', 'ping RTT', 'TTFB - PRET 2ND LOAD'])
+    plt.legend([ 'ping RTT', 'TTFB - PRET', 'TTFB - PRET 2ND LOAD', 'TTFB - PRET 3RD LOAD'])
     ax1 = plt.subplot(111)
-    total = [x for x in RTT] + [x for x in RTTv2] + [x for x in pR]
+    total = [x for x in RTTv3] + [x for x in RTT] + [x for x in RTTv2] + [x for x in pR]
     perc = np.percentile(total, 95)
 
     ax1.set_xlim([0, perc])
@@ -155,5 +163,5 @@ def create_plot_cdf(site = "aggregatelog.txt", savefig = False, showfig = False
     plt.show() if showfig else {plt.close()}
     return ax1
 
-main()
 makeFiles()
+main()
