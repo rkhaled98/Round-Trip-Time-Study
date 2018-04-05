02a0f08c93f225e97e808f8c2be69973f46cd252
Rafi Khaled <rkhaled@vassar.edu>
2018-04-05 00:37:12 -0400

    for loop for plotting columns in create_plot_cdf

--- analysis.py
+++ analysis.py
@@ -15,11 +15,11 @@ def main():
     #create_plot_all(savefig = True)
     #create_plot_cdf(showfig = True)
     #create_plot_cdf(showfig = True, day = "2018-02-11 Sunday")
-    #create_plot_violin("www.google.com")
+    create_plot_violin(site = "aggregate")
     #create_plot_violin()
     #create_plot_cdf(showfig = True, savefig = True)
     #create_plot_cdf(showfig = True, day = "2018-02-13 Tuesday")
-    #create_plot_cdf(site = "www.google.com", showfig = True)
+    create_plot_cdf(site = "www.google.com", showfig = True)
     #create_plot_cdf(site = "www.google.com", showfig = True)
 
 def clean_csv(site = "aggregate"):
@@ -32,10 +32,9 @@ def clean_csv(site = "aggregate"):
     df.pingRTT = df.pingRTT.astype('float64')
     f = open('dataframe_out', 'w')
     f.write(df.to_csv())
+    for col in df.columns:
+        print(df[col])
     return df
-
-def get(site):
-    print(site)
 
 '''
 #the purpose of this function is to create
@@ -109,27 +108,10 @@ def get_values(site = "newtestaggregatelog.txt", day = ""):
     f.close()
     return (RTTv3s, RTTs, RTTv2s, pR)
 
-
+'''
 def get_location(site):
     cwd = os.getcwd()
     return cwd + '/logs/' + site
-'''
-#the purpose of this function is to
-#cast each value in the array to a float
-#and then add this to a new array,
-#which is returned. it doesn't
-#actually do any sorting, but this
-#should be added??
-def sort_and_cast(arr):
-    new_arr = []
-    for val in arr:
-        try:
-            f_val = (float(val))
-            new_arr.append(f_val)
-        except:
-            print('error')
-
-    return new_arr
 
 def create_plot_all(savefig = False, showfig = False):
     wd = os.getcwd() + "/logs/"
@@ -151,19 +133,14 @@ def create_plot_all(savefig = False, showfig = False):
 
 def create_plot_cdf(site, showfig = False, savefig = False):
     data = clean_csv(site)
-    print(data)
-    #x = np.sort(data.RTT)
-    #print(x)
+    data = data.loc[:, ['RTT', 'RTTtwo', 'RTTthree', 'pingRTT']]
     y = np.arange(1, len(data.RTT)+1) / len(data.RTT)
-    print(data.pingRTT)
-    print(data.RTT)
-    print(data.RTTtwo)
-    _ = plt.plot(np.sort(data.pingRTT), np.linspace(0, 1, len(data.pingRTT), endpoint=False))
-    _ = plt.plot(np.sort(data.RTT), np.linspace(0, 1, len(data.RTT), endpoint=False))
-    _ = plt.plot(np.sort(data.RTTtwo), np.linspace(0, 1, len(data.RTTtwo), endpoint=False))
-    _ = plt.plot(np.sort(data.RTTthree), np.linspace(0, 1, len(data.RTTthree), endpoint=False))
-
-    #_ = plt.plot(x, y, marker='.', linestyle='none')
+    for col in data.columns:
+        _ = plt.plot(np.sort(data[col]), np.linspace(0, 1, len(data[col]), endpoint=False))
+    #_ = plt.plot(np.sort(data.pingRTT), np.linspace(0, 1, len(data.pingRTT), endpoint=False))
+    #_ = plt.plot(np.sort(data.RTT), np.linspace(0, 1, len(data.RTT), endpoint=False))
+    #_ = plt.plot(np.sort(data.RTTtwo), np.linspace(0, 1, len(data.RTTtwo), endpoint=False))
+    #_ = plt.plot(np.sort(data.RTTthree), np.linspace(0, 1, len(data.RTTthree), endpoint=False))
     _ = plt.xlabel('RTT')
     _ = plt.ylabel('ECDF')
     _ = plt.legend([ 'ping RTT', 'TTFB - PRET', 'TTFB - PRET 2ND LOAD', 'TTFB - PRET 3RD LOAD'])
@@ -172,84 +149,16 @@ def create_plot_cdf(site, showfig = False, savefig = False):
     plt.savefig('graphs/CDF-of-' + site.replace('www.','') + '.png') if savefig else {}
     plt.show() if showfig else {plt.close()}
 
-def create_plot_cdf_o(site = "newtestaggregatelog.txt", savefig = False, showfig = False
-, day = ""):
-    '''
-    vals = get_values(site, day)
-
-    RTTv3 = sort_and_cast(vals[0])
-    RTT = sort_and_cast(vals[1])
-    RTTv2 = sort_and_cast(vals[2])
-    pR = sort_and_cast(vals[3])
-    '''
-    vals = clean_csv(site)
-    vals = vals.loc[:, ['RTT', 'RTTtwo', 'RTTthree']]#, 'pingRTT']]
-    #vals.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-
-    #ser = pd.Series(vals.RTTtwo)
-    #(df - df.mean()) / (df.max() - df.min())
-
-    #ser.hist(cumulative=True, normed=1, bins=100)
-
-    #ax1 = plt.subplot(111)
-    #ax1.set_xlim([0, 500])
-    #plt.show()
-    #vals.RTT = vals.RTT.sort_values()
-    #vals.RTTtwo = vals.RTTtwo.sort_values()
-    #vals.RTTthree = vals.RTTthree.sort_values()
-    #vals.pingRTT = vals.pingRTT.sort_values()
-
-    #cum_dist = np.linspace(0.,1.,len(vals.RTT))
-    #ser_cdf = pd.Series(cum_dist, index=vals.RTT)
-
-    #ser_cdf.plot(drawstyle='steps')
-
-    vals = vals.cumsum()
-    plt.figure; vals.plot();
-    with pd.plotting.plot_params.use('x_compat', True):
-        vals.RTT.plot(color='r')
-        vals.RTTtwo.plot(color='g')
-        vals.RTTthree.plot(color='b')
-    plt.show()
-    '''
-
-    plt.plot(np.sort(vals.pingRTT), np.linspace(0, 1, len(vals.pingRTT), endpoint=False))
-    plt.plot(np.sort(vals.RTT), np.linspace(0, 1, len(vals.pingRTT), endpoint=False))
-    plt.plot(np.sort(vals.RTTtwo), np.linspace(0, 1, len(vals.pingRTT), endpoint=False))
-    plt.plot(np.sort(vals.RTTthree), np.linspace(0, 1, len(vals.pingRTT), endpoint=False))
-
-    plt.xlabel('RTT (ms)')
-    plt.ylabel('CDF')
-    if site == 'newtestaggregatelog.txt':
-        plt.title('Aggregate CDF for RTTs of the top 100 websites (%i)' #on %s
-        % (vals.shape[0]))
-    else:
-        plt.title('CDF for RTTs of %s (%i) on %s'
-        % (site, vals.shape[0], day))
-    #plt.legend(['TTFB - PRET', 'ping RTT', 'TTFB - PRET 2ND LOAD'])
-    plt.legend([ 'ping RTT', 'TTFB - PRET', 'TTFB - PRET 2ND LOAD', 'TTFB - PRET 3RD LOAD'])
-    ax1 = plt.subplot(111)
-    total = vals.sum(axis=1)
-    perc = np.percentile(total, 95)
-
-    ax1.set_xlim([0, perc])
-    ax1.grid(color='#C0C0C0', linestyle='-', linewidth=1,)
-    plt.savefig('graphs/CDF OF RTTs ' + site.replace('www.','') + '.png') if savefig else {}
-    plt.show() if showfig else {plt.close()}
-    return ax1
-    '''
-
-
 def create_plot_violin(site = "aggregate", day = ""):
     vals = clean_csv(site)
     sns.set_style("whitegrid")
 
-    vals = vals.loc[:, ['RTTtwo', 'RTT', 'RTTthree', 'pingRTT']]
+    vals = vals.loc[:, ['RTT', 'RTTtwo', 'RTTthree', 'pingRTT']]
     #tips = sns.load_dataset(vals)
     ax = plt.subplot(111)
     ax = sns.violinplot(data=vals)
     #total =
-    ax.set_ylim([0, 100])
+    ax.set_ylim([0, 1000])
     plt.show()
     '''
     vals = get_values(site, day)
