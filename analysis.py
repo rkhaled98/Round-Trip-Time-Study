917bda8d64e3b8ab2a2f6da24460300a5805900b
Rafi Khaled <rkhaled@vassar.edu>
2018-04-05 02:11:57 -0400

    clean up

--- analysis.py
+++ analysis.py
@@ -13,10 +13,10 @@ def main():
     #create_plot_cdf(site = "aggregate", showfig = True)
     #vals = get_values()
     #create_plot_all(savefig = True)
-    create_plot_all(showfig = True)
+    #create_plot_all(showfig = True)
     #create_plot_cdf(showfig = True, day = "2018-02-11 Sunday")
     #create_plot_violin(site = "aggregate")
-    #create_plot_violin()
+    create_plot_violin("www.alibaba.com")
     #create_plot_cdf(showfig = True, savefig = True)
     #create_plot_cdf(showfig = True, day = "2018-02-13 Tuesday")
     #create_plot_cdf(site = "www.google.com", showfig = True)
@@ -43,7 +43,6 @@ def makeFiles(file = "aggregate.txt"):
     wd = os.getcwd() # this is where to get the files
     data = clean_csv() # get the values from the aggregate
     sites = data.sitename.unique() # get the unique sitenames
-    print(sites)
     for site in sites: # for each site
         f = open(wd + '/logs/' + site, 'w') # create new file with sitename in logs
         f.write(data[data.sitename.str.match('^' + site + '$')].to_csv()) # and write site specific data
@@ -63,7 +62,7 @@ def create_plot_cdf(site, showfig = False, savefig = False):
     for col in data.columns:
         _ = plt.plot(np.sort(data[col]), np.linspace(0, 1, len(data[col]), endpoint=False))
     _ = plt.ylabel('ECDF')
-    _ = plt.legend([ 'ping RTT', 'TTFB - PRET', 'TTFB - PRET 2ND LOAD', 'TTFB - PRET 3RD LOAD'])
+    _ = plt.legend(['ping RTT', 'TTFB - PRET', 'TTFB - PRET 2ND LOAD', 'TTFB - PRET 3RD LOAD'])
     ax1 = plt.subplot(111)
     total = [data[x] for x in data]
     xlim_max = np.percentile(total, 97.5)
@@ -72,16 +71,15 @@ def create_plot_cdf(site, showfig = False, savefig = False):
     plt.show() if showfig else {plt.close()}
 
 def create_plot_violin(site = "aggregate", day = ""):
-    vals = clean_csv(site)
+    data = clean_csv(site)
+    data = data.loc[:, ['RTT', 'RTTtwo', 'RTTthree', 'pingRTT']]
     sns.set_style("whitegrid")
-
-    vals = vals.loc[:, ['RTT', 'RTTtwo', 'RTTthree', 'pingRTT']]
-    #tips = sns.load_dataset(vals)
-    ax = plt.subplot(111)
-    ax = sns.violinplot(data=vals)
-    #total =
-    ax.set_ylim([0, 1000])
+    sns.violinplot(data=data)
+    ax1 = plt.subplot()
+    total = [data[x] for x in data]
+    ylim_max = np.percentile(total, 97.5)
+    ax1.set_ylim([0, ylim_max])
     plt.show()
 
-makeFiles()
+#makeFiles()
 main()
