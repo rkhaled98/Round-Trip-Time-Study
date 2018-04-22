6734413047c1a0a8c5828a85c26a8f92e72781fa
Rafi Khaled <rkhaled@vassar.edu>
2018-04-22 01:12:03 -0400

    somewhat temp plot line work

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -11,7 +11,8 @@ import os
 def main():
     #makeFiles()
     #clean_csv('www.google.com', day=['2018-03-25', '2018-03-24'])
-    clean_csv(show_hours = True)
+    #clean_csv(show_hours = True)
+    create_plot_temporal(site = "aggregate", show_hours = True)
     #create_plot_cdf(site = "aggregate", showfig = True)
     #create_plot_all(savefig = True)
     #create_plot_all(showfig = True)
@@ -29,18 +30,28 @@ def clean_csv(site = "aggregate", day = ['all'], show_hours = False):
     df = pd.read_csv("aggregate.txt")
     df = df.loc[:, ['tstamp', 'sitename', 'RTT', 'RTTtwo', 'RTTthree', 'pingRTT']]
     df = df if site == "aggregate" else df[df.sitename.str.match('^' + site + '$')] #regex for getting the exact site if specified
-    df = df if day == ['all'] else df[df.tstamp.str.contains('|'.join([x for x in day])) if len(day) > 1 else df.tstamp.str.contains(day[0])] #this line isolates specific days as specified by the array day otherwise it shows all the days if default value of ['all'] is used for days
+    #df = df if day == ['all'] else df[df.tstamp.str.contains('|'.join([x for x in day])) if len(day) > 1 else df.tstamp.str.contains(day[0])] #this line isolates specific days as specified by the array day otherwise it shows all the days if default value of ['all'] is used for days
     df = df[~df.pingRTT.str.match("None")]
     df.pingRTT.apply(lambda val: float(val))
     df.pingRTT = df.pingRTT.astype('float64')
     if show_hours:
-        df_hours = pd.DataFrame(columns=[str(x) for x in range(0,25,1)])
-        print(df_hours)
-        for i in range(0, 25, 1):
-            hour = "0" + str(i) if i < 10 else str(i)
-            print(hour)
-            df_hours[str(i)] = df[df.tstamp.str.contains('.*\s' + hour)] # + ':\d\d:\d*$')
+        d = {}
+        #df = df.loc[:, ['tstamp', 'RTTtwo']]
+        for x in range(0, 25, 1):
+            hour = "0" + str(x) if x < 10 else str(x)
+            d[hour] = df[df.tstamp.str.contains('.*\s' + hour)]
+        return d
+
+
+    '''
+    df_hours = pd.DataFrame(columns=[str(x) for x in range(0,25,1)])
+    print(df_hours)
+    for i in range(0, 25, 1):
+        hour = "0" + str(i) if i < 10 else str(i)
+        print(hour)
+        df_hours[str(i)] = df[df.tstamp.str.contains('.*\s' + hour)] # + ':\d\d:\d*$')
         print(df_hours["0"])
+        '''
     #f = open('dataframe_out', 'w')
     #f.write(df.to_csv())
     return df
@@ -64,8 +75,8 @@ def create_plot_all(savefig = False, showfig = False, day = ['all']):
         f = open(wd + site) # open individual site
         create_plot_cdf(site, showfig, savefig)
 
-def create_plot_cdf(site, showfig = False, savefig = False, day = ['all']):
-    data = clean_csv(site)
+def create_plot_cdf(site, showfig = False, savefig = False, day = ['all'], show_hours = False):
+    data = clean_csv(site, show_hours)
     data = data.loc[:, ['RTT', 'RTTtwo', 'RTTthree', 'pingRTT']] # only want the relevant columns in dataframe
     y = np.arange(1, len(data.RTT)+1) / len(data.RTT)
     for col in data.columns:
@@ -77,6 +88,18 @@ def create_plot_cdf(site, showfig = False, savefig = False, day = ['all']):
     xlim_max = np.percentile(total, 97.5)
     ax1.set_xlim([0, xlim_max])
     plt.savefig('graphs/CDF-of-' + site.replace('www.','') + '.png') if savefig else {}
+    plt.show() if showfig else {plt.close()}
+
+def create_plot_temporal(site, showfig = True, savefig = False, day = ['all'], show_hours = True):
+    data = clean_csv(site, show_hours = True)
+    #data = dict(data)
+    #print(data)
+    for key, value in data.iteritems():
+        df = data[key]
+        _ = plt.plot(np.sort(df['RTTtwo']), np.linspace(0, 1, len(df['RTTtwo']), endpoint=False))
+    _ = plt.ylabel('ECDF')
+    _ = plt.legend([key for key, value in data.iteritems()])
+    ax1 = plt.subplot(111)
     plt.show() if showfig else {plt.close()}
 
 def create_plot_violin(site = "aggregate", day = ""):
